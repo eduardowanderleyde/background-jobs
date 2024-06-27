962af8c99ac1903bf903f99301afa3052c6046be
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-06-26 23:53:14 -0300

    birthday mail works

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+  def welcome_email(user_id)
+    @user = User.find(user_id)
+    @url = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def birthday(user_id)
+    @user = User.find(user_id)
+    mail(to: @user.email, subject: 'Happy Birthday! #{@user.email}')
+  end
+end
